ceafc0ea7414635352b1b7ef0173e854ca0a735f
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-10-15 09:24:22 -0500

    add ClienteController for handling client-related API requests

---
+++ client-service/src/main/java/codes/alem/clientservice/controller/ClienteController.java
@@ -0,0 +1,34 @@
+package codes.alem.clientservice.controller;
+
+import codes.alem.clientservice.dto.ClienteDto;
+import codes.alem.clientservice.entity.Cliente;
+import codes.alem.clientservice.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("/api/clientes")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+
+    @GetMapping("/{codigoUnico}")
+    public Mono<ResponseEntity<ClienteDto>> getClienteByCodigoUnico(@PathVariable String codigoUnico) {
+        return clienteService.findByCodigoUnico(codigoUnico)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/1/{codigoUnico}")
+    public Mono<ResponseEntity<Cliente>> getClienteByCodigoUnico1(@PathVariable String codigoUnico) {
+        return clienteService.findByCodigoUnico1(codigoUnico)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+}
