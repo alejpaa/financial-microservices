586415bca56267078bf707c22d736edcc96028b2
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-10-14 21:20:26 -0500

    add eureka server

---
+++ eureka-server/src/test/java/codes/alem/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.alem.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
