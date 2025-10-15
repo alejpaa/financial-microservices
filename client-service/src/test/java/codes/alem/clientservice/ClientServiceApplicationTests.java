6d26db6a8467c34bae49b3a105d457c725239fe1
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-10-14 22:03:27 -0500

    add client-service

---
+++ client-service/src/test/java/codes/alem/clientservice/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.alem.clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
